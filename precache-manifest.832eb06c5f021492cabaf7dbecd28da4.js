18350cd56e1eef882f0e0139d21aca28609aeaf4
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-02-23 01:50:44 +0200

    Updates

---
+++ precache-manifest.832eb06c5f021492cabaf7dbecd28da4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba57c94510b683c7c2136a5ad4b0e8f",
+    "url": "/Hangman-With-React/index.html"
+  },
+  {
+    "revision": "63bbd5b788e5292ea3b7",
+    "url": "/Hangman-With-React/static/css/main.9b865b2d.chunk.css"
+  },
+  {
+    "revision": "e3c0e9a4425e06ea56b9",
+    "url": "/Hangman-With-React/static/js/2.d1aef30d.chunk.js"
+  },
+  {
+    "revision": "63bbd5b788e5292ea3b7",
+    "url": "/Hangman-With-React/static/js/main.b65e4325.chunk.js"
+  },
+  {
+    "revision": "d575f7b5df9d1b34f3e6",
+    "url": "/Hangman-With-React/static/js/runtime~main.51cc1a7a.js"
+  }
+]);
